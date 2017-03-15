6ea41ca4945e54827d64f2490f455142fb6445fb
Mauricio Medina <mauricio.medina.munoz@gmail.com>
2017-03-15 15:19:53 -0300

    uploading  pinterest to github  because i just did it to heroku when following the tutorials... pollo

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :sign_up, only: [:mail, :password, :password_confirmation, :phone]
+
+  protected
+
+  def configure_permitted_parameters
+  	params.require(:sign_up).permit(:email, :password, :password_confirmation, :phone)
+  end
+
 end
--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,5 +1,18 @@
 class PinsController < ApplicationController
   before_action :set_pin, only: [:show, :edit, :update, :destroy]
+  before_filter :authenticate_user!, except:[:index, :show]
+
+  def pinsof
+    @user_id = params[:user_id]
+    @user = User.find(@user_id)
+    @pins = @user.pins
+    
+  end
+
+  def mypins
+    @pins = current_user.pins
+
+  end
 
   # GET /pins
   # GET /pins.json
@@ -25,6 +38,8 @@ class PinsController < ApplicationController
   # POST /pins.json
   def create
     @pin = Pin.new(pin_params)
+    @pin.user_id = current_user
+
 
     respond_to do |format|
       if @pin.save
@@ -69,6 +84,6 @@ class PinsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def pin_params
-      params.require(:pin).permit(:photo, :description)
+      params.require(:pin).permit(:photo, :description, :name)
     end
 end
